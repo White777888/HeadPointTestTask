2b77ad6523244d6ef936aa6cb39b268bfa431aa5
IlyaBelovLan <olderperson1920@gmail.com>
2023-02-22 22:39:18 +0300

    Add project files.

---
+++ HeadPointTestTask/AnagramGrouping.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HeadPointTestTask
+{
+    public static class AnagramGrouping
+    {
+        public static string[][] GroupAnagrams(IEnumerable<string> anargrams)
+        {
+            return anargrams
+                .GroupBy(OrderedAnagramInLowercase)
+                .Select(group => group.ToArray())
+                .ToArray();
+        }
+
+        private static string OrderedAnagramInLowercase(string anagram) =>
+            string.Concat(anagram.ToLower().OrderBy(ch => ch));
+    }
+}
---
+++ Tests/AnagramGroupingTests.cs
@@ -0,0 +1,62 @@
+﻿using HeadPointTestTask;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Tests
+{
+    internal class AnagramGroupingTests
+    {
+        [Test]
+        public void GroupsAnagrams()
+        {
+            var anagrams = new[] { "ток", "рост", "кот", "торс", "кто", "фывап", "рок" };
+            var expectedAnagramGroups = new[] {
+                new[] { "ток", "кот", "кто" },
+                new[] { "рост", "торс" },
+                new[] { "фывап" },
+                new[] { "рок" }
+            };
+
+            var actualAnagramGroups = AnagramGrouping.GroupAnagrams(anagrams);
+
+            Assert.IsTrue(AnagramGroupsAreEqual(expectedAnagramGroups, actualAnagramGroups));
+        }
+
+        [Test]
+        public void CaseInsensitive()
+        {
+            var anagrams = new[] { "ток", "Рост", "кот", "торс", "Кто", "фывап", "рок" };
+            var expectedAnagramGroups = new[] {
+                new[] { "ток", "кот", "Кто" },
+                new[] { "Рост", "торс" },
+                new[] { "фывап" },
+                new[] { "рок" }
+            };
+
+            var actualAnagramGroups = AnagramGrouping.GroupAnagrams(anagrams);
+
+            Assert.IsTrue(AnagramGroupsAreEqual(expectedAnagramGroups, actualAnagramGroups));
+        }
+
+        [Test]
+        public void EmptySequenceIsEmptyResult()
+        {
+            var emptyAnagrams = new string[0];
+
+            var actualAnagramGroups = AnagramGrouping.GroupAnagrams(emptyAnagrams);
+
+            Assert.IsEmpty(actualAnagramGroups);
+        }
+
+        private static bool AnagramGroupsAreEqual(string[][] first, string[][] second)
+        {
+            if (first.Length != second.Length)
+                return false;
+
+            return Enumerable
+                .Range(0, first.Length)
+                .All(i => first[i].SequenceEqual(second[i]));
+        }
+    }
+}
